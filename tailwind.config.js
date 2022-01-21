56e187dd2820c15886ef0c7a65add0a92797f4b8
henriquevc <henriquevc93@gmail.com>
2022-01-21 10:29:05 -0300

    primeiro protótipo. adicionando fonts, aos e imagens ao site

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#9668ff',
+        secondary: '#ea6cbd',
+        dark: '#1d1d1d',
+        background: '#121212',
+        accent: '#909090'
+      }
+    },
+  },
+}
