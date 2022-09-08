58623e1c55f4dda09877e58612f82f5fb6c22cf4
henriquevc <henriquevc93@gmail.com>
2022-09-08 17:39:42 -0300

    spinner no botao de enviar do cta

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,7 @@ const btnCtaWhatsapp = document.getElementById('btn-cta-whatsapp')
 const btnCtaEnviar = document.getElementById('btn-cta-enviar')
 const inputTelefone = document.getElementById('telefone')
 
+
 if (inputTelefone) {
     VMasker(inputTelefone).maskPattern('(99) 99999-9999')
 }
@@ -77,6 +78,8 @@ btnCtaEnviar.addEventListener('click', () => {
     }
     let titlePage = document.title
 
+    toggleButtonEnviar()
+
     // Decrypt
     var bytes = CryptoJS.AES.decrypt('U2FsdGVkX19LnNqqfCDPKKIWu5mnph44PKD+jenSqMQvVWxO6Nz1vdO+vWWQpk3DLBNwoBrUMw/XXGP4oa1Tpw==', 'henriquecarvalho')
     var secretKey = bytes.toString(CryptoJS.enc.Utf8)
@@ -95,11 +98,13 @@ Ramos: ${textoRamos}
 Tela que o cliente estava: ${titlePage}
 ${this.planoSelecionado ? 'Plano Selecionado: ' + this.planoSelecionado.nome + ' - R$' + this.planoSelecionado.valorMensal : ''}`,
     }).then(() => {
+        toggleButtonEnviar()
         if (modalCta) {
             (toggleModal())()
         }
         alert('Recebemos os seus dados e já entraremos em contato! Obrigado')
     }).catch(error => {
+        toggleButtonEnviar()
         alert('Não foi possível enviar o contato. Fale com a gente pelo número (11) 4040-8065')
         console.error('Email não enviado. Erro: ' + error)
     })
@@ -109,6 +114,20 @@ function toggleModal () {
     return () => {
         modalCta.classList.toggle('flex')
         modalCta.classList.toggle('hidden')
+    }
+}
+
+function toggleButtonEnviar () {
+    const textoEl = btnCtaEnviar.children[0]
+    const loadingEl = btnCtaEnviar.children[1]
+    textoEl.classList.toggle('hidden')
+    loadingEl.classList.toggle('flex')
+    loadingEl.classList.toggle('hidden')
+    if (!loadingEl.classList.contains('hidden')) {
+        btnCtaEnviar.setAttribute('disabled', 'disabled')
+    }
+    else {
+        btnCtaEnviar.removeAttribute('disabled')
     }
 }
 
